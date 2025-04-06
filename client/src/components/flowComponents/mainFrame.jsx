cf55aef9070c64343a280cb8b099d611d3da5055
Abdullah <abdul.abdullah@mekari.com>
2025-04-06 17:01:39 +0700

    feat: none

--- client/src/components/flowComponents/mainFrame.jsx
+++ client/src/components/flowComponents/mainFrame.jsx
@@ -1,6 +1,6 @@
-"use client";
+'use client'
 
-import React, { useCallback } from "react";
+import React, { useCallback } from 'react'
 import {
   ReactFlow,
   useNodesState,
@@ -10,37 +10,31 @@ import {
   Background,
   BackgroundVariant,
   ReactFlowProvider,
-} from "@xyflow/react";
-import "@xyflow/react/dist/style.css";
-import { MagicWandIcon } from "@radix-ui/react-icons";
+} from '@xyflow/react'
+import '@xyflow/react/dist/style.css'
+import { MagicWandIcon } from '@radix-ui/react-icons'
 
-const nodeTypes = {};
+const nodeTypes = {}
 
 export default function MainFrame() {
-  const [nodes, setNodes] = useNodesState([]);
-  const [edges, setEdges] = useEdgesState([]);
+  const [nodes, setNodes] = useNodesState([])
+  const [edges, setEdges] = useEdgesState([])
 
   const addNode = useCallback(() => {
     const newNode = {
       id: (nodes.length + 1).toString(),
-      type: "default", // Use "default" or your custom node type
+      type: 'default', // Use "default" or your custom node type
       position: { x: Math.random() * 400, y: Math.random() * 400 },
       data: { label: `Node ${nodes.length + 1}` },
-    };
-    setNodes((nds) => [...nds, newNode]);
-  }, [nodes, setNodes]);
+    }
+    setNodes((nds) => [...nds, newNode])
+  }, [nodes, setNodes])
 
   return (
     <ReactFlowProvider>
-      <div
-        style={{ width: "100vw", height: "80vh", backgroundColor: "#101013" }}
-      >
+      <div style={{ width: '100vw', height: '80vh', backgroundColor: '#101013' }}>
         <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes}>
-          <Background
-            color="#E5E5E5"
-            gap={60}
-            variant={BackgroundVariant.Dots}
-          />
+          <Background color="#E5E5E5" gap={60} variant={BackgroundVariant.Dots} />
           <Controls position="top-left" orientation="horizontal">
             <ControlButton onClick={addNode}>
               <MagicWandIcon />
@@ -49,5 +43,5 @@ export default function MainFrame() {
         </ReactFlow>
       </div>
     </ReactFlowProvider>
-  );
+  )
 }
